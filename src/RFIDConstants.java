102c29b83e333326eb02a182691fb571ff909ba0
Colin Scott <cs@mhw.cs.washington.edu>
2011-04-27 10:54:51 -0700

    Added new header types

--- src/RFIDConstants.java
+++ src/RFIDConstants.java
@@ -7,7 +7,8 @@
 public class RFIDConstants {
 
     //Add or change frame components as needed
-    public static final byte ACK = 'a';
-    public static final byte QUERY = 'q';
-    public static final byte WINDOW = 'w';
+    public static final byte NEW_QUERY = 'n';       // the previous query was succesful (implicit ACK)
+    public static final byte COLLISION_QUERY = 'c'; // there was a collision in the previous round (tell tags to decrease window)
+    public static final byte DESPERATE_QUERY = 'd'; // no tags responded last time, so everyone should decrease their window size
+    public static final byte WINDOW_QUERY = 'w';    // The reader is telling the tags to explicity set their window size
 }
